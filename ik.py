dae3e04302aae8f26b69412c0c12aaa15381698b
leonidk <lkeselman@gmail.com>
2018-01-30 20:19:41 -0500

    better codepath

---
+++ ik.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import argparse
+import iksolver as ik
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Solve ik for N joint arm',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--links', nargs='+', help="link lengths",default=[2,2,2])
+    parser.add_argument('--target', nargs='+', help="target position and quat",default=[3,3,3, 1,0,0,0])
+    parser.add_argument('--method', help="solver method", choices=['Powell', 'SLSQP', 'COBYLA', 'CMA'],default='SLSQP')
+    parser.add_argument('--grad', dest='grad', action='store_true',help='use the analytical jacobian')
+    parser.add_argument('--obs', nargs='*', help="obstacles 4xM values",default=[1,1,1, 0.5])
+    parser.add_argument('--random', dest='random', action='store_true',help='use random init')
+
+    parser.set_defaults(grad=False)
+    parser.set_defaults(random=False)
+
+    args = parser.parse_args()
+    N = len(args.links)
+    pi = 3.14159
+    link_lengths = [int(x) for x in args.links] #[2 for _ in range(N)]
+    min_roll     = [-pi for _ in range(N)]
+    max_roll     = [+pi for _ in range(N)]
+    min_pitch    = [-pi for _ in range(N)]
+    max_pitch    = [+pi for _ in range(N)]
+    min_yaw      = [-pi/2.0 for _ in range(N)]
+    max_yaw      = [+pi/2.0 for _ in range(N)]
+    obstacles    = list(chunks([float(x) for x in args.obs],4))
+
+    target = [float(x) for x in args.target]
+    
+    ik.method = args.method
+    ik.use_grad = args.grad
+    ik.initial_x = [0.0 for _ in range(N*3)]
+    if args.random:
+        import random
+        ik.initial_x = [random.uniform(-pi,pi) for _ in range(N)] + [random.uniform(-pi,pi) for _ in range(N)] + [random.uniform(-pi/2.0,pi/2.0) for _ in range(N)] 
+ 
+    res = ik.solve(target,link_lengths,min_roll,max_roll,min_pitch,max_pitch,min_yaw,max_yaw,obstacles)
+
+    import matplotlib as mpl
+    mpl.use('Qt5Agg')
+    mpl.rcParams['legend.fontsize'] = 10
+
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    import numpy as np
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    x0 = np.hstack(res)#[0,0,0, pi/4,pi/4,pi/4, 0,0,0]
+    # plot lines
+    pos = np.array([0,0,0])
+    qM = np.eye(3)
+    rs = x0[:N]
+    ps = x0[N:2*N]
+    ys = x0[2*N:]
+    ll = link_lengths
+    for r,p,y,l in zip(rs,ps,ys,ll):
+        pos0 = pos
+        pos,qM = ik.fwd(pos,l,r,p,y,qM)
+        ax.plot([pos0[0],pos[0]], [pos0[1],pos[1]],[pos0[2],pos[2]])
+
+    # plot spheres
+    for o in obstacles:
+        u = np.linspace(0, 2 * np.pi, 10)
+        v = np.linspace(0, np.pi, 10)
+        x = o[3] * np.outer(np.cos(u), np.sin(v)) + o[0]
+        y = o[3] * np.outer(np.sin(u), np.sin(v)) + o[1]
+        z = o[3] * np.outer(np.ones(np.size(u)), np.cos(v)) + o[2]
+        ax.plot_surface(x, y, z, color='b')
+
+    # plot goal
+    ax.scatter(target[0], target[1], target[2], c='r')
+
+    ax.legend()
+    ax.set_xlim(0,5)
+    ax.set_ylim(0,5)
+    ax.set_zlim(0,5)
+    plt.show()
---
+++ iksolver.py
@@ -0,0 +1,157 @@
+import sys
+import os
+
+if True:
+    import autograd.numpy as np
+    import transforms3d_grad as transforms3d
+    from autograd import grad, jacobian, hessian, jacobian, hessian_vector_product
+    from autograd import elementwise_grad as egrad
+else: #not using grad path
+    import transforms3d
+    import numpy as np
+import scipy.optimize as opt
+
+# from http://paulbourke.net/geometry/circlesphere/sphere_line_intersection.py
+def sphere_line_intersection(l1, l2, sp, r):
+
+    def square(f):
+        return f * f
+    #from math import sqrt
+
+    # l1[0],l1[1],l1[2]  P1 coordinates (point of line)
+    # l2[0],l2[1],l2[2]  P2 coordinates (point of line)
+    # sp[0],sp[1],sp[2], r  P3 coordinates and radius (sphere)
+    # x,y,z   intersection coordinates
+    #
+    # This function returns a pointer array which first index indicates
+    # the number of intersection point, followed by coordinate pairs.
+
+    p1 = p2 = None
+
+    a = square(l2[0] - l1[0]) + square(l2[1] - l1[1]) + square(l2[2] - l1[2])
+    b = 2.0 * ((l2[0] - l1[0]) * (l1[0] - sp[0]) +
+               (l2[1] - l1[1]) * (l1[1] - sp[1]) +
+               (l2[2] - l1[2]) * (l1[2] - sp[2]))
+
+    c = (square(sp[0]) + square(sp[1]) + square(sp[2]) + square(l1[0]) +
+            square(l1[1]) + square(l1[2]) -
+            2.0 * (sp[0] * l1[0] + sp[1] * l1[1] + sp[2] * l1[2]) - square(r))
+
+    i = b * b - 4.0 * a * c
+
+    if i < 0.0:
+        pass  # no intersections
+    elif i == 0.0:
+        # one intersection
+        p[0] = 1.0
+
+        mu = -b / (2.0 * a)
+        p1 = (l1[0] + mu * (l2[0] - l1[0]),
+              l1[1] + mu * (l2[1] - l1[1]),
+              l1[2] + mu * (l2[2] - l1[2]),
+              )
+
+    elif i > 0.0:
+        # first intersection
+        mu = (-b + np.sqrt(i)) / (2.0 * a)
+        p1 = (l1[0] + mu * (l2[0] - l1[0]),
+              l1[1] + mu * (l2[1] - l1[1]),
+              l1[2] + mu * (l2[2] - l1[2]),
+              )
+
+        # second intersection
+        mu = (-b - np.sqrt(i)) / (2.0 * a)
+        p2 = (l1[0] + mu * (l2[0] - l1[0]),
+              l1[1] + mu * (l2[1] - l1[1]),
+              l1[2] + mu * (l2[2] - l1[2]),
+              )
+
+    return p1, p2
+
+
+def fwd(start,length,r,p,y,qM):
+    M = transforms3d.euler.euler2mat(r,p,y,'sxyz')
+    return start+np.array([length,0,0]).dot(M), qM.dot(M)
+
+def solve(target, link_length, min_roll, max_roll, min_pitch, max_pitch, min_yaw, max_yaw, obstacles):
+    """Function that uses optimization to do inverse kinematics for a snake robot
+
+    Args:
+        target:  [x, y, z, q0, q1, q2, q3]' position and orientation of the end effector
+        link_length:  Nx1 vectors of the lengths of the links
+        min_xxx, max_xxx are the vectors of the limits on the roll, pitch, yaw of each link.
+        obstacles: A Mx4 matrix where each row is [ x y z radius ] of a sphere obstacle.
+        M obstacles.
+
+    Returns:
+        r: N vector of roll
+        p: N vector of pitch
+        y: N vector of yaw
+
+    """
+    N = len(link_length)
+    def func(x0):
+        pos = np.array([0,0,0])
+        qM = np.eye(3)
+        rs = x0[:N]
+        ps = x0[N:2*N]
+        ys = x0[2*N:]
+        ll = link_length
+        for r,p,y,l in zip(rs,ps,ys,ll):
+            pos,qM = fwd(pos,l,r,p,y,qM)
+        t = np.array(target)
+        C = 1 # meters and radians. Close enough
+        extra = 0.0
+        for ob in obstacles:
+            p0 = np.array([0,0,0])
+            q = np.eye(3)
+            for r,p,y,l in zip(rs,ps,ys,ll):
+                p1,q = fwd(p0,l,r,p,y,q)
+                i1,i2 = sphere_line_intersection(p0,p1,ob[:3],ob[3])
+                p0 = p1
+                if i1 is not None:
+                    i1 = np.array(i1)
+                    extra += (((ob[:3]-i1)**2-ob[3])**2).sum()
+                if i2 is not None:
+                    i2 = np.array(i2)
+                    extra += (((ob[:3]-i2)**2-ob[3])**2).sum()
+        quat = transforms3d.quaternions.mat2quat(qM)
+        rot_error = 1.0 - ((quat*np.array([t[3],-t[4],-t[5],-t[6]]))**2 ).sum()
+        return ((pos[:3]-t[:3])**2).sum() + C*rot_error + extra
+
+    bounds =          [(x,y) for x,y in zip(min_roll, max_roll)]
+    bounds = bounds + [(x,y) for x,y in zip(min_pitch,max_pitch)]
+    bounds = bounds + [(x,y) for x,y in zip(min_yaw,  max_yaw)]
+    print('BOUNDS',bounds)
+    #midpoint = lambda mn,mx: mn+0.5*(mx-mn)
+    #x0 = [midpoint(min_roll[i],max_roll[i]) for i in range(N)] + [midpoint(min_pitch[i],max_pitch[i]) for i in range(N)] + [midpoint(min_yaw[i],  max_yaw[i]) for i in range(N)] 
+    
+    x0 = np.array(initial_x) + 1e-6
+    constraints = []
+    
+    if use_grad:
+        jac = grad(func)
+        #hess = hessian(func)
+        def jac_reg(x):
+            j = jac(x)
+            if np.isfinite(j).all():
+                return j
+            else:
+                return opt.approx_fprime(x,func,1e-6)
+        jacobian = jac_reg
+        #print('USING A.GRAD')
+    else:
+        jacobian = None
+        #print('USING N.GRAD')
+
+    if method == 'CMA':
+        import cma
+        es = cma.CMAEvolutionStrategy(x0, 3.141592/2.0, {'bounds':list(zip(*bounds)),'verb_log':0})
+        es.optimize(func)
+        #print(es.result_pretty())
+        resx = es.result.xbest
+    else:
+        res = opt.minimize(func,x0=x0,bounds=bounds,constraints=constraints,method=method,jac=jacobian)
+        print(res)
+        resx = res.x
+    return resx[:N], resx[N:2*N], resx[2*N:]
